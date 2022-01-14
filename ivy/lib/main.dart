8387c39cf4f07dc84be84503dde6c9db6868cacd
Cameron Barker <cameronbarkeran@gmail.com>
2022-01-14 21:38:23 +0000

    firebase setup

--- ivy/lib/main.dart
+++ ivy/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -26,7 +32,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Ivy'),
+        title: const Text('Ivy'),
       ),
     );
   }
