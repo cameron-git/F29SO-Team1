160d450d4ae973932158de09890ee65f2e58caba
Cameron Barker <cameronbarkeran@gmail.com>
2022-01-15 21:35:26 +0000

    remove google

--- ivy/lib/main.dart
+++ ivy/lib/main.dart
@@ -2,13 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutterfire_ui/auth.dart';
-import 'package:google_sign_in_web/google_sign_in_web.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 import 'firebase_options.dart';
 
 // importing other source code files
-import 'package:ivy/home_screen.dart';
+import 'home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
