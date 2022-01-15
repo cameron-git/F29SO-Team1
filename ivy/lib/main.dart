1f6c63fa9d11b9c933d1f0c9857fde2d941395d3
Cameron Barker <cameronbarkeran@gmail.com>
2022-01-15 23:31:44 +0000

    refactor

--- ivy/lib/main.dart
+++ ivy/lib/main.dart
@@ -1,12 +1,10 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutterfire_ui/auth.dart';
 
 import 'firebase_options.dart';
 
 // importing other source code files
-import 'home_screen.dart';
+import 'screens/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,51 +25,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       home: MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  const MainPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return const HomePage();
-        } else {
-          return const SignInScreen(
-            providerConfigs: [
-              EmailProviderConfiguration(),
-            ],
-          );
-        }
-      },
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Ivy'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.logout),
-            tooltip: 'Sign Out',
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-          ),
-        ],
-      ),
-      body: HomeScreen(),
     );
   }
 }
---
+++ ivy/lib/screens/home.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ivy'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.logout),
+            tooltip: 'Sign Out',
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+          ),
+        ],
+      ),
+      body: HomeScreen(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return HomePage();
+        } else {
+          return SignInScreen(
+            providerConfigs: [
+              EmailProviderConfiguration(),
+            ],
+            headerBuilder: (context, constraints, _) => Padding(
+              padding: EdgeInsets.all(20),
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: Image.asset('assets/ivy-logo.png'),
+              ),
+            ),
+            footerBuilder: (context, action) => Padding(
+              padding: EdgeInsets.all(12),
+              child: Text('ToS applies!!!'),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('assets/ivy-logo.png'),
+    );
+  }
+}
