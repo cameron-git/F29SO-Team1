fa43774aacc177a42d98dc11676e76027ec08f0a
Laura <laura.schauer212@gmail.com>
2022-01-16 13:45:55 +0000

    created bottom navigation bar to navigate between pages

--- ivy/lib/screens/home.dart
+++ ivy/lib/screens/home.dart
@@ -107,6 +107,7 @@ class _HomeScreenState extends State<HomeScreen> {
       onPageChanged: (index) {
         onPageChanged(index);
       },
+      // the pages to navigate between
       children: [
         Feed(),
         Search(),
