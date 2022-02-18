ad4cc71c75cb0fad35aecfd5a92d0489fab154a7
Cameron Barker <cameronbarkeran@gmail.com>
2022-02-18 23:11:05 +0000

    merge

--- ivy/lib/screens/post.dart
+++ ivy/lib/screens/post.dart
@@ -31,13 +31,9 @@ class _PostState extends State<Post> {
         }
         Map<String, dynamic> data =
             snapshot.data!.data() as Map<String, dynamic>;
-<<<<<<< HEAD
-        List<dynamic> tags = data['tags'];
-=======
-        if(data['tags'] == Null){
+        if (data['tags'] == Null) {
           data['tags'] = "NA";
         }
->>>>>>> 22581d5f4336a842b43072ace77238dd2701f625
         return Scaffold(
           appBar: AppBar(
             title: Row(
@@ -131,13 +127,8 @@ class _PostState extends State<Post> {
               SizedBox(
                 height: 50,
                 child: ListView(
-                  children: tags.map((tag) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Chip(
-                        label: Text(tag),
-                      ),
-                    );
+                  children: data['tags'].map((tag) {
+                    return Text(tag);
                   }).toList(),
                   scrollDirection: Axis.horizontal,
                 ),
@@ -192,11 +183,7 @@ class _NewPostState extends State<NewPost> {
               TextField(
                 controller: _tagController,
                 decoration: const InputDecoration(
-<<<<<<< HEAD
                   labelText: 'Tags (space separated)',
-=======
-                  labelText: 'Tags (separated by commas)',
->>>>>>> 22581d5f4336a842b43072ace77238dd2701f625
                   border: OutlineInputBorder(),
                 ),
               ),
