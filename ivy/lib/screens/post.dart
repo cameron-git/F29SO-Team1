fd33a6126d178241e47bf13fc84639a8650c8333
Cameron Barker <cameronbarkeran@gmail.com>
2022-02-22 21:56:47 +0000

    firebase changes

--- ivy/lib/screens/post.dart
+++ ivy/lib/screens/post.dart
@@ -1,5 +1,6 @@
 // Should contain all post stuff and create new post widget
 
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
