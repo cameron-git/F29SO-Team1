bd9e44e08f4def6fe51fd928215b334d1f69d736
(Raymond) Pyra Firkins <rjf2@hw.ac.uk>
2022-03-02 16:51:41 +0000

    tags are capitalised

--- ivy/lib/screens/post.dart
+++ ivy/lib/screens/post.dart
@@ -246,7 +246,7 @@ class _PostState extends State<Post> {
                                       <String, dynamic>{
                                         'title': _titleController.text,
                                         'description': _descController.text,
-                                        'tags': _tagsController.text.split(" "),
+                                        'tags': _tagsController.text.toUpperCase().split(" "),
                                       },
                                       SetOptions(merge: true),
                                     );
@@ -511,7 +511,7 @@ class _NewPostState extends State<NewPost> {
                       ],
                       'title': _titleController.text,
                       'description': _descController.text,
-                      'tags': _tagController.text.split(' '),
+                      'tags': _tagController.text.toUpperCase().split(' '),
                     },
                   );
                   Navigator.pop(context);
