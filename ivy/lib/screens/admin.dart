16de6075cb449291fbb41c30b5818ccb6724eb23
Cameron Barker <cameronbarkeran@gmail.com>
2022-03-16 18:37:37 +0000

    admin.dart

---
+++ ivy/lib/screens/admin.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdminUI extends StatelessWidget {
+  const AdminUI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
