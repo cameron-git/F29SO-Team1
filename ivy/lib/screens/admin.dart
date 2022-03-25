1c548cb7344ee14fa01dc4bfcbc7e44eaea43e97
Cameron Barker <cameronbarkeran@gmail.com>
2022-03-25 16:29:58 +0000

    1

--- ivy/lib/screens/admin.dart
+++ ivy/lib/screens/admin.dart
@@ -1,7 +1,3 @@
-import 'dart:html';
-import 'dart:ui';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart ';
 import 'package:ivy/auth.dart';
 import 'package:ivy/screens/post.dart';
