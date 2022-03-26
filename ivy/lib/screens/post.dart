41a4ad8136d4e88a910aeef8eb9c2d52717663d6
Laura <laura.schauer212@gmail.com>
2022-03-26 15:15:26 +0000

    added image for audio

--- ivy/lib/screens/post.dart
+++ ivy/lib/screens/post.dart
@@ -646,9 +646,24 @@ class _PostState extends State<Post> {
       );
     } else if (mediaType == "mp3") {
       return Container(
-        height: 200,
-        width: 500,
-        color: Colors.grey,
+        alignment: Alignment.center,
+        child: Container(
+          width: 500.0,
+          height: 200.0,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(
+                  "https://images.pexels.com/photos/3784221/pexels-photo-3784221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                ),
+                fit: BoxFit.cover,
+                opacity: 0.5),
+          ),
+          child: Center(
+              child: Text(
+            media['url'],
+            textAlign: TextAlign.center,
+          )),
+        ),
       );
     } else if (mediaType == "mp4") {
       return Container(
@@ -661,9 +676,6 @@ class _PostState extends State<Post> {
           videoURL: media['url'],
           inDrawerList: true,
         ),
-        // child: Center(
-        //   child: Text(media['url']),
-        // ),
       );
     }
   }
